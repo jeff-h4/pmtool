aa1f9dd06b7f47188e062be64f23b4c36e6017be
Jeff H <jahuangi@yahoo.ca>
2015-08-30 23:29:59 -0700

    added basic project functionality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,48 @@
 class ProjectsController < ApplicationController
+
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @project = Project.new
+  end
+
+  def create 
+    @project = Project.new project_params
+    if @project.save
+      redirect_to project_path(@project), notice: "Project created successfully!"
+    else
+      flash[:alert] = "Project not created. See errors below:"
+      render :new
+    end
+  end
+
+  def index 
+    @projects = Project.all
+  end
+
+  def show 
+  end
+
+  def edit 
+  end
+
+  def update
+    if @project.update project_params 
+      redirect_to project_path(@project), notice: "Project updated successfully!"
+    else
+      flash[:alert] = "Project update failed! See errors below:"
+      render :edit
+    end
+  end
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+  private
+  def project_params
+    params.require(:project).permit(:title,:description,:due_date)
+  end
+  def find_project
+    @project = Project.find params[:id]
+  end
 end
